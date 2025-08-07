60c4a871bc42e075465bf36cc5975d134fd62632
jasmine6983 <jasmine.m@techversantinfotech.com>
2025-08-07 17:33:27 +0530

    get custom menu from wordpress api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
+import axios from "axios";
 import logo from "../assets/img/Logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [menuItems, setMenuItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://expertnext.demoserver.work/wp-json/custom/v1/menu/primary`) // or REACT_APP_URL if using CRA
+      .then((res) => {
+        setMenuItems(res.data); // depends on plugin response structure
+      })
+      .catch((err) => {
+        console.error("Failed to load menu:", err);
+      });
+  }, []);
   return (
     <header className="header">
       <div className="container-fluid">
@@ -18,35 +30,20 @@ const Header = () => {
             <div className="header__nav">
               <nav className="header__menu mobile-menu">
                 <ul>
-                  <li>
-                    <a href="/">Home</a>
-                  </li>
-
-                  <li>
-                    <a href="#">Categories</a>
-                  </li>
-                  <li>
-                    <a href="#">Pages</a>
-                    <ul className="dropdown">
-                      <li>
-                        <a href="/blog-details">Blogs</a>
-                      </li>
-                      <li>
-                        <a href="/contact">About</a>
-                      </li>
-                    </ul>
-                  </li>
-
-                  <li>
-                    <Link to="/contact">Contact</Link>
-                  </li>
+                  {menuItems.map((item) => (
+                    <li key={item.ID}>
+                      <a href={item.url}>{item.title}</a>
+                    </li>
+                  ))}
                 </ul>
               </nav>
               <div className="header__menu__right">
-                <a href="https://expertnext.demoserver.work/wp-login.php" className="primary-btn">
+                <a
+                  href="https://expertnext.demoserver.work/wp-login.php"
+                  className="primary-btn"
+                >
                   <i className="fa fa-plus"></i> Login
                 </a>
-
               </div>
             </div>
           </div>
